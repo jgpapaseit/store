851474669e64ffa16f72d0edb9583a93c069791f
jorge <jordi@creactivitat.com>
2023-01-31 20:48:16 +0100

    test message

--- src/js/Product.test.js
+++ src/js/Product.test.js
@@ -1,6 +1,6 @@
 import Product from "./Product.js";
 
-test('Return 1', async () => {
+test('Return product id', async () => {
 	const id = 10
 	const data = await Product(id)
 	expect(data.id).toBe(id)
